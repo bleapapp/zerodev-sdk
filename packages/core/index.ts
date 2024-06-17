69f8c81c589c7afc19bc7a3a58115aa4428556b9
yaniferhaoui <yani.ferhaoui@hotmail.fr>
2024-06-17 10:42:36 +0200

    feat: declare export getKernelV3Nonce

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -51,6 +51,7 @@ export { KernelV3_1AccountAbi } from "./accounts/kernel/abi/kernel_v_3_1/KernelA
 export { KernelV3FactoryAbi } from "./accounts/kernel/abi/kernel_v_3_0_0/KernelFactoryAbi.js"
 export { KernelFactoryStakerAbi } from "./accounts/kernel/abi/kernel_v_3_0_0/KernelFactoryStakerAbi.js"
 export { TokenActionsAbi } from "./accounts/kernel/abi/TokenActionsAbi.js"
+export { getKernelV3Nonce } from "./accounts/index.js"
 export * as constants from "./constants.js"
 export * from "./utils.js"
 export { gasTokenAddresses, type TokenSymbolsMap } from "./gasTokenAddresses.js"
