a4853b454b52a1cb64c6b6df16d9ea8e0de3ad32
yaniferhaoui <yani.ferhaoui@hotmail.fr>
2024-06-20 17:47:34 +0200

    feat: export utils

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,13 +1,4 @@
 export {
-    createKernelAccount,
-    createKernelV1Account,
-    createKernelV2Account,
-    type KernelSmartAccount,
-    KERNEL_ADDRESSES,
-    addressToEmptyAccount,
-    EIP1271Abi
-} from "./accounts/index.js"
-export {
     sponsorUserOperation,
     type SponsorUserOperationParameters,
     type SponsorUserOperationReturnType
@@ -42,7 +33,6 @@ export {
 export { KernelV3FactoryAbi } from "./accounts/kernel/abi/kernel_v_3_0_0/KernelFactoryAbi.js"
 export { KernelFactoryStakerAbi } from "./accounts/kernel/abi/kernel_v_3_0_0/KernelFactoryStakerAbi.js"
 export { TokenActionsAbi } from "./accounts/kernel/abi/TokenActionsAbi.js"
-export { getKernelV3Nonce } from "./accounts/index.js"
 export * as constants from "./constants.js"
 export * from "./utils.js"
 export { gasTokenAddresses, type TokenSymbolsMap } from "./gasTokenAddresses.js"
@@ -51,3 +41,4 @@ export {
     getCustomNonceKeyFromString
 } from "./accounts/utils/index.js"
 export { KernelEIP1193Provider } from "./providers/index.js"
+export * from "./accounts/index.js"
